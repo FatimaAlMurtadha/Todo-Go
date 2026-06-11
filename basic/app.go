bef006b87d986dcda3f00c62df0635a5950dccde
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-06-11 10:47:41 +0200

    init variables

--- basic/app.go
+++ basic/app.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
     fmt.Println("Hello, Fatima!")
+
+	var myName string = "Hello from the first string"
+	fmt.Println(myName)
+
+	var myNumber int = 33
+	fmt.Println("Fatima is ",  myNumber , " years old")
+
+	// unit8 - unit16 - unit32 - unit64  -- Unsigned bit
+	// int8  - int16  - int32  - int64   -- Signed   bit
 }
